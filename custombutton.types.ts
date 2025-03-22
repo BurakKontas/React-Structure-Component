855f4038f8902c33d0127a12f79fd4ec121a3f8b
Arda Burak Kontaş <aburakkontas@trakya.edu.tr>
2025-03-22 13:44:43 +0300

    Update custombutton.types.ts

--- custombutton.types.ts
+++ custombutton.types.ts
@@ -1,10 +1,6 @@
 import * as React from 'react';
 
 export interface CustomButtonProps {
-    onClick?: () => void;
-    disabled?: boolean;
-    className?: string;
-    style?: React.CSSProperties;
-    children?: React.ReactNode;
+    
 }
 
